d43030da9a35f83bef22a785fa68dc2f77cbaee7
Your Name <you@example.com>
2025-04-29 13:15:29 +0500

    save 4 (idk)

--- lesson 3/main.py
+++ lesson 3/main.py
@@ -1,29 +1,51 @@
+import random
+import time
+
 import pygame as pg
+
 pg.init()
-SIZE = WIDTH, HEIGHT = 800, 600
+SIZE = WIDTH, HEIGHT = 800, 800
 screen = pg.display.set_mode(SIZE)
+
 from my_sprite import MySprite
+from my_sprite import Python
+from utils import load_img
 
 FPS = 60
 BG_COLOR = pg.Color('black')
-clock = pg.time.Clock()
-
-all_sprites = pg.sprite.Group()
-player = MySprite(all_sprites)
-player.rect.bottom = HEIGHT
-player.rect.centerx = WIDTH / 2
+PHON = pg.transform.smoothscale(load_img('war.jpg'), SIZE)
 
 
-running = True
-while running:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            running = False
-    screen.fill(BG_COLOR)
+def game():
+    all_sprites = pg.sprite.Group()
+    player = MySprite(all_sprites)
+    player.rect.bottom = HEIGHT
+    player.rect.centerx = WIDTH / 2
+    python = Python(all_sprites)
+    python_group = pg.sprite.Group()
+    clock = pg.time.Clock()
+    running = True
+    start = time.time()
+    while running:
+        events = pg.event.get()
+        for event in events:
+            if event.type == pg.QUIT:
+                running = False
+        screen.blit(PHON, (0, 0))
 
-    all_sprites.draw(screen)
-    all_sprites.update()
+        all_sprites.draw(screen)
+        all_sprites.update(events)
+        collide = pg.sprite.spritecollideany(player, python_group, None)
+        if collide != None:
+            print(time.time() - start)
+            break
+        if random.random() < 0.07:
+            Python(all_sprites, python_group)
 
-    pg.display.flip()
-    clock.tick(FPS)
+        pg.display.flip()
+        clock.tick(FPS)
+
+
+while True:
+    game()
 pg.quit()
