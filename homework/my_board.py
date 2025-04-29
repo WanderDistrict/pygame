20466b70fb7cfd8dcddaee30d64d48b85c3703ff
WanderDistrict <pavelegorov13579@gmail.com>
2025-04-29 17:37:00 +0500

    save 5 (idk)

--- homework/my_board.py
+++ homework/my_board.py
@@ -1,9 +1,17 @@
 import pygame as pg
+import random
 
 from board import Board
 
 
 class Game(Board):
+    def __init__(self, width: int, height: int, left: int = 10, top: int = 10, cell_size: int = 40):
+        super().__init__(width, height, left, top, cell_size, initial_value=0)
+        for row in range(height):
+            for col in range(width):
+                self._board[row][col] = random.randint(0, 1)
+        self.current_player = 0
+
     def draw_cell(self, screen: pg.Surface, row: int, col: int, rect: pg.Rect) -> None:
         if self._board[row][col] == 0:
             color = pg.Color('white')
@@ -11,7 +19,13 @@ class Game(Board):
             color = pg.Color('black')
         pg.draw.circle(screen, color, rect.center, rect.w * 0.4)
 
-    def on_click(self, row: int, col: int) -> None:\
-        # TODO: turn check
-        # it's an example. Code should be different
-        self._board[row][col] = 1 - self._board[row][col]
+    def on_click(self, row: int, col: int) -> None:
+        if self._board[row][col] == self.current_player:
+            return
+
+        for i in range(self._width):
+            self._board[row][i] = self.current_player
+        for i in range(self._height):
+            self._board[i][col] = self.current_player
+
+        self.current_player = 1 - self.current_player
